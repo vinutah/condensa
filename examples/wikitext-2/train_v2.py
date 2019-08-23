1f50fbf6f23d6e0b68d7978a18e7a3fd95dcac54
Vinu Joseph <vinu@cs.utah.edu>
2019-08-23 16:47:38 -0600

    func_cersion

---
+++ examples/wikitext-2/train_v2.py
@@ -0,0 +1,15 @@
+
+import sys
+import argparse
+
+def parse_args(arguments):
+    
+    
+
+
+def main(arguments):
+    parse_args(arguments)
+
+
+if __name__ == '':
+    sys.exit(main(sys.argv[1:0])
