d8266e1a8d9779da7ad64543ff6858ecbe1e0480
Vinu Joseph <vinu@cs.utah.edu>
2019-08-27 09:09:19 -0600

    updated

---
+++ examples/wikitext-2/compress_transformers_ngc.py
@@ -0,0 +1,37 @@
+
+import sys
+import argparse
+import subprocess as subp
+
+def parse_args(arguments):
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description="Debugging Transformer Compression on NGC")
+    parser.add_argument('--command', help='sleep' )
+    return parser.parse_args(arguments)
+  
+def call_ngc(name, command):
+    ngc_command = [
+        'ngc', 'batch', 'run', 
+        '--name', name, 
+        '--total-runtime', '168h',
+        '--image', 'nvidia/pytorch:19.04-py3', 
+        '--ace', 'nv-us-west-2',
+        '--instance', 'dgx1v.16g.8.norm',
+        '--commandline', 'sleep 168h', 
+        '--result', '/results',
+        '--datasetid', '13254:/imagenet',
+        '--workspace', 'condensa-transformer:/condensa:RW',
+        '--port', '8888',
+    ]
+    print(ngc_command)
+    subp.check_call(ngc_command)
+
+def main(arguments):
+    args = parse_args(arguments)
+    print(args)
+    call_ngc('debug', args.command)
+
+if __name__ == '__main__':
+  sys.exit(main(sys.argv[1:]))
+  
