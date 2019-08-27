29ad06a11e7c26d5ff457429bc95fbcf204fcf4c
Vinu Joseph <vinu@cs.utah.edu>
2019-08-27 10:34:16 -0600

    updated with data loading methods

--- examples/wikitext-2/compress.py
+++ examples/wikitext-2/compress.py
@@ -80,22 +80,77 @@ def parse_args(arguments):
     return parser.parse_args(arguments)
 
 
-def batchify(data, bsz):
-    # Work out how cleanly we can divide the dataset into bsz parts.
-    nbatch = data.size(0) // bsz
-    
+def batchify(data, batch_size, device):
+    """
+    Starting from sequential data, batchify arranges the dataset into columns.
+    For instance, with the alphabet as the sequence and batch size 4, we'd get
+    ┌ a g m s ┐
+    │ b h n t │
+    │ c i o u │
+    │ d j p v │
+    │ e k q w │
+    └ f l r x ┘.
+    These columns are treated as independent by the model, which means that the
+    dependence of e. g. 'g' on 'f' can not be learned, but allows more efficient
+    batch processing.
+    >>> train_data = batchify(corpus.train, train_batch_size, device)
+    >>> val_data = batchify(corpus.valid, eval_batch_size, device)
+    >>> test_data = batchify(corpus.test, eval_batch_size, device)
+    """
+    # Work out how cleanly we can divide the dataset into batch_size parts.
+    nbatch = data.size(0) // batch_size
     # Trim off any extra elements that wouldn't cleanly fit (remainders).
-    data = data.narrow(0, 0, nbatch * bsz)
-    
-    # Evenly divide the data across the bsz batches.
-    data = data.view(bsz, -1).t().contiguous()
+    data = data.narrow(0, 0, nbatch * batch_size)
+    # Evenly divide the data across the batch_size batches.
+    data = data.view(batch_size, -1).t().contiguous()
     return data.to(device)
 
-def get_batch(source, i):
-    seq_len = min(args.bptt, len(source) - 1 - i)
-    data = source[i:i+seq_len]
-    target = source[i+1:i+1+seq_len].view(-1)
-    return data, target
+class BatchLoader:
+    """
+    subdivides the source data into chunks of length
+    args.bptt.  If source is equal to the example output of the
+    batchify function, with a bptt-limit of 2, we'd get the
+    following two Variables for i = 0:
+
+    ┌ a g m s ┐ ┌ b h n t ┐
+    └ b h n t ┘ └ c i o u ┘
+
+    Note that despite the name of the function, the subdivison of
+    data is not done along the batch dimension (i.e. dimension 1),
+    since that was handled by the batchify function. The chunks
+    are along dimension 0, corresponding to the seq_len dimension
+    in the LSTM.
+    >>> train_loader = BatchLoader(train_data, bptt)
+    >>> val_loader   = BatchLoader(val_data, bptt)
+    >>> test_loader  = BatchLoader(test_data, bptt)
+    """
+    def __init__(self, data, bptt):
+        self.data = data
+        self.bptt = bptt
+    def __len__(self):
+        return len(self.data) - 1
+    def __iter__(self):
+        for i in range(0, len(self), self.bptt):
+            seq_len = min(self.bptt, len(self) - i)
+            data = self.data[i:i+seq_len]
+            target = self.data[i+1:i+1+seq_len].view(-1)
+            yield data, target
+
+def make_data_loaders(corpus, device, train_batch_size, eval_batch_size, bptt):
+        """
+        Returns three loaders: (train_loder, val_loader, test_loader)
+        >>> train_loader, val_loader, test_loader = make_data_loaders( corpus, device,
+                                                                       args.batch_size, 
+                                                                       args.eval_batch_size, 
+                                                                       args.bptt)
+        """
+        train_data = batchify(corpus.train, train_batch_size, device)
+        val_data = batchify(corpus.valid, eval_batch_size, device)
+        test_data = batchify(corpus.test, eval_batch_size, device)
+
+        train_loader = BatchLoader(train_data, bptt)
+        val_loader = BatchLoader(val_data, bptt)
+        test_loader = BatchLoader(test_data, bptt)
 
 def create_scheme(name):
     if args.scheme == 'PRUNE':
